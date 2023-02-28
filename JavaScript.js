9a04c49efa74404f02337319b1c5002224b67ed5
Vaishali Vaidya <vaishalivaidya1996@gmail.com>
2023-02-28 18:51:09 +0530

    add

--- JavaScript.js
+++ JavaScript.js
@@ -3,6 +3,7 @@ const start = document.getElementById("start")
 const pause = document.getElementById("pause")
 const reset = document.getElementById("reset")
 const cast = document.getElementById("cast")
+const cast_display = document.getElementById("cast-display")
 let mlsec = 0
 let hour = 0;
 let second = 0
@@ -11,13 +12,16 @@ let interval = null
 start.addEventListener("click", start_timer)
 pause.addEventListener("click", pause_timer)
 reset.addEventListener("click", Stop_timer)
+// cast.addEventListener("click", Cast_timer)
 function Timer_Watch() {
     mlsec++
     if (mlsec === 1000) {
         mlsec = `${000}`
         second++
         if (second === 60) {
-            second = `${00}`
+            if (second < 10) {
+                second = "0" + second
+            }
             min++
             if (min === 60) {
                 min = `${00}`
@@ -47,8 +51,16 @@ function pause_timer() {
 }
 function Stop_timer() {
     pause_timer();
+    pause.innerText = "Pause"
     mlsec = 0
     second = 0
     min = 0
     watch.innerHTML = `00 : 00 : 00 : 000`
+
 }
+cast.addEventListener("click", ()=>{
+    let li = document.createElement("li")
+    cast_display.appendChild(li)
+    li.innerHTML = watch.innerHTML
+    // cast_display.innerHTML =
+})
